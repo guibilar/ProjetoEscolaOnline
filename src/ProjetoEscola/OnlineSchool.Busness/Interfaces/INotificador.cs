7d92f900675e14c1c1cd02e2ceee24aff82ba7e0
Guilherme R. Bilar <guilherme.bilar@bitzar.solutions>
2021-01-21 22:30:41 -0300

    Estruturação de camada de negocios

---
+++ src/ProjetoEscola/OnlineSchool.Busness/Interfaces/INotificador.cs
@@ -0,0 +1,12 @@
+﻿using OnlineSchool.Busness.Notificacoes;
+using System.Collections.Generic;
+
+namespace OnlineSchool.Busness.Interfaces
+{
+    public interface INotificador
+    {
+        bool TemNotificacao();
+        List<Notificacao> ObterNotificacoes();
+        void Handle(Notificacao notificacao);
+    }
+}
